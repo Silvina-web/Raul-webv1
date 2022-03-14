f9f96fdd1a5f5dee1f926f5f869e577dd09fb987
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-03-14 14:06:59 -0300

    Modal

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ $('#zoom_05').ezPlus({
   zoomType: 'inner',
   cursor: 'crosshair'
 });
+
+
+const open = document.getElementById('open');
+const ventana = document.getElementById('ventana');
+const close = document.getElementById('close');
+
+open.addEventListener('click', ()=>{
+  ventana.classList.add('show');
+  
+  
+});
+
+close.addEventListener('click', ()=>{
+  ventana.classList.remove('show');
+});
+
+
+
