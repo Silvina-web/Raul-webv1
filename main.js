7a95b98ed8da8d885551bbcd63a6887bf39e2056
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-03-15 14:01:05 -0300

    movile

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ $('#zoom_05').ezPlus({
 });
 
 
+$('#zoom_06').ezPlus({
+  zoomType: 'inner',
+  cursor: 'crosshair'
+});
+
 const open = document.getElementById('open');
 const ventana = document.getElementById('ventana');
 const close = document.getElementById('close');
